1e27fab25f7e9b504a8808e19d4a8ba64bf2cdda
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-08 21:28:45 +0530

    Card data added

---
+++ Configurable UI/src/config/config.js
@@ -0,0 +1,22 @@
+const CardInfo = [
+    {
+        id: 1,
+        title: 'Rose',
+        description: 'Classic and fragrant, roses come in many colors and are symbols of love and beauty.',
+        image: ''
+    },
+
+    {
+        id: 2,
+        title: 'Sunflower',
+        description: 'Large, bright yellow flowers with a prominent central disk, known for their cheerful appearance and association with the sun',
+        image: ''
+    },
+
+    {
+        id: 3,
+        title: 'Lily',
+        description: 'Elegant and fragrant, lilies are known for their large, trumpet-shaped flowers and diverse color variations.',
+        image: ''
+    },
+]
---
+++ Configurable UI/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import CardApp from './component/CardApp'
+
+createRoot(document.getElementById('root')).render(
+<CardApp/>
+)
