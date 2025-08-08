067b34419c8d07b9f873ef7ba9a28b270dd39c76
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-08 21:34:14 +0530

    another card data added

--- Configurable UI/src/config/config.js
+++ Configurable UI/src/config/config.js
@@ -19,4 +19,24 @@ const CardInfo = [
         description: 'Elegant and fragrant, lilies are known for their large, trumpet-shaped flowers and diverse color variations.',
         image: ''
     },
+
+    {
+        id: 4,
+        title: 'Tulip',
+        description: 'A cup-shaped flower with vibrant colors, tulips are a popular spring flower and symbolize perfect love.',
+        image: ''
+    },
+
+    {
+        id: 5,
+        title: 'Jasmine',
+        description: 'A fragrant, star-shaped flower, jasmine is often used in perfumes and teas.',
+        image: ''
+    },
+
+    {
+        id: 6,
+        title: 'Lavender',
+        description: 'Known for its calming fragrance and purple blooms, lavender is used in aromatherapy and perfumes.',
+    }
 ]
